1842b07a0ab9eac31704dd91f5aa63ee8b148908
3A532024 <kc1223127@gmail.com>
2018-11-05 09:08:47 +0800

    設定autoload，自動載入Carbon

--- public/index.php
+++ public/index.php
@@ -1,60 +1,135 @@
 <?php
+require __DIR__.'/bootstrap.php';
+require __DIR__.'/vendor/autoload.php';
+//date_default_timezone_set('Asia/Taipei');
+use Carbon\Carbon;
+Carbon::setLocale('zh-tw');
+// connect to dabase
+try {
+    $dsn = 'mysql:host='.DB_HOST.';port='.DB_PORT.';dbname='.DB_DATABASE.';charset='.DB_CHARSET;
+    $pdo = new PDO($dsn, DB_USERNAME, DB_PASSWORD);
+} catch (PDOException $e) {
+    echo  "Error: ".$e->getMessage()."＜br/＞";
+    die();
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
 
-/**
- * Laravel - A PHP Framework For Web Artisans
- *
- * @package  Laravel
- * @author   Taylor Otwell <taylor@laravel.com>
- */
+<head>
 
-define('LARAVEL_START', microtime(true));
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
 
-/*
-|--------------------------------------------------------------------------
-| Register The Auto Loader
-|--------------------------------------------------------------------------
-|
-| Composer provides a convenient, automatically generated class loader for
-| our application. We just need to utilize it! We'll simply require it
-| into the script here so that we don't have to worry about manual
-| loading any of our classes later on. It feels great to relax.
-|
-*/
+    <title>Laravel 道場訓練教材：運用 Carbon 顯示資料庫內文章時間</title>
 
-require __DIR__.'/../vendor/autoload.php';
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
 
-/*
-|--------------------------------------------------------------------------
-| Turn On The Lights
-|--------------------------------------------------------------------------
-|
-| We need to illuminate PHP development, so let us turn on the lights.
-| This bootstraps the framework and gets it ready for use, then it
-| will load up this application so that we can run it and send
-| the responses back to the browser and delight our users.
-|
-*/
+    <!-- Custom CSS -->
+    <link href="css/3-col-portfolio.css" rel="stylesheet">
 
-$app = require_once __DIR__.'/../bootstrap/app.php';
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
 
-/*
-|--------------------------------------------------------------------------
-| Run The Application
-|--------------------------------------------------------------------------
-|
-| Once we have the application, we can handle the incoming request
-| through the kernel, and send the associated response back to
-| the client's browser allowing them to enjoy the creative
-| and wonderful application we have prepared for them.
-|
-*/
+</head>
 
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+<body>
 
-$response = $kernel->handle(
-    $request = Illuminate\Http\Request::capture()
-);
+<!-- Navigation -->
+<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+    <div class="container">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">Laravel 道場</a>
+        </div>
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav">
+                <li>
+                    <a href="http://www.laravel-dojo.com/about/dojo" target="_blank">關於道場</a>
+                </li>
+                <li>
+                    <a href="http://www.laravel-dojo.com/workshops" target="_blank">工作坊</a>
+                </li>
+                <li>
+                    <a href="http://www.laravel-dojo.com/contact" target="_blank">聯絡我們</a>
+                </li>
+            </ul>
+        </div>
+        <!-- /.navbar-collapse -->
+    </div>
+    <!-- /.container -->
+</nav>
 
-$response->send();
+<!-- Page Content -->
+<div class="container">
 
-$kernel->terminate($request, $response);
+    <!-- Page Header -->
+    <div class="row">
+        <div class="col-lg-12">
+            <h1 class="page-header">文章清單
+                <small><?php echo Carbon::now()->toDateString(); ?></small>
+            </h1>
+        </div>
+    </div>
+    <!-- /.row -->
+
+    <!-- Projects Row -->
+    <div class="row">
+        <?php $statement = $pdo->query("SELECT * FROM `posts` ORDER BY `created_at` DESC LIMIT 3"); ?>
+        <?php while($row = $statement->fetch(PDO::FETCH_OBJ)): ?>
+            <div class="col-md-4 portfolio-item">
+                <a href="#">
+                    <img class="img-responsive" src="http://placehold.it/700x400" alt="">
+                </a>
+                <h3>
+                    <a href="#"><?=$row->title?></a>
+                </h3>
+                <p><?=mb_substr($row->content, 0, 130, "utf-8"),'…'?></p>
+                <p class="text-right">
+                    <span class="glyphicon glyphicon-time"></span>
+                    發表於 <?=Carbon::createFromFormat('Y-m-d H:i:s', $row->created_at)->diffForHumans()?>
+                </p>
+            </div>
+        <?php endwhile; ?>
+    </div>
+    <!-- /.row -->
+
+    <hr>
+
+    <!-- Footer -->
+    <footer>
+        <div class="row">
+            <div class="col-lg-12">
+                <p>Copyright &copy; Laravel 道場訓練教材  2015</p>
+            </div>
+        </div>
+        <!-- /.row -->
+    </footer>
+
+</div>
+<!-- /.container -->
+
+<!-- jQuery -->
+<script src="js/jquery.js"></script>
+
+<!-- Bootstrap Core JavaScript -->
+<script src="js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
